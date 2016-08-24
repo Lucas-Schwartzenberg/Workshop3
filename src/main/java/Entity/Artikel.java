89b757e6a8abf4c407c95eff5729c46c839342e1
Lucas-Schwartzenberg <lucas.Schwartzenberg@xs4all.nl>
2016-08-24 20:17:41 +0200

    Merge branch 'master' of https://github.com/prutgers/test1 into prutgers-master
    Conflicts:
    	JavaApplication60/src/javaapplication60/JavaApplication60.java

--- src/main/java/Entity/Artikel.java
+++ src/main/java/Entity/Artikel.java
@@ -12,6 +12,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -39,8 +41,8 @@ public class Artikel implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
-    @NotNull
     @Column(name = "artikel_id")
     private Integer artikelId;
     @Basic(optional = false)
--- src/main/webapp/RestClientArtikelLuc.js
+++ src/main/webapp/RestClientArtikelLuc.js
@@ -6,7 +6,18 @@
 var rootURL = "http://localhost:8080/Workshop7/webresources/artikel";
 
 $(document).ready(function(){
-findAll();});
+findAll();
+
+    $("button#btnDelete").click(function(event){
+    event.preventDefault();
+	if ($('#artikelId').val() === '')
+            alert("Selecteer eerst een artikel om te verwijderen");
+	else
+            alert("Delete function not implemented");
+		deleteArtikel();
+	return false;
+    });
+});
 
 function findAll() {
 	console.log('findAll Artikel');
@@ -22,12 +33,14 @@ function renderList(data) {
                 <th>Artikel</th>\n\
                 <th>Prijs</th>\n\
                 <th>Voorraad</th>\n\
+                <th>Verwijderen</th>\n\
                 </tr>");
 	$.each(list, function(index, artikel) {
 		$('#List').append('<tr><td id="selectArtikel"><button id='+ artikel.artikelId +'>'+artikel.artikelId+'</td>\n\
                                     <td>'+artikel.artikelNaam+'</td>\n\
                                     <td>'+artikel.artikelPrijs+',-</td>\n\
                                     <td>'+artikel.artikelVoorraad+'</td>\n\
+                                    <td id="verwijderArtikel"><button id='+ artikel.artikelId +'>'+artikel.artikelId+'</td>\n\
                                     </li>');
 	});
 }
@@ -37,33 +50,49 @@ $(document).on("click", "td#selectArtikel button", function(event){
     findById(event.target.id);
 });
 
+$(document).on("click", "td#verwijderArtikel button", function(event){
+    event.preventDefault();
+    deleteArtikel(event.target.id);
+});
+
 function findById(Id) {
     $.getJSON(rootURL+"/"+Id, function(result){
         $('#artikelId').val(result.artikelId);
         $('#artikelNaam').val(result.artikelNaam);
         $('#artikelVoorraad').val(result.artikelVoorraad);
         $('#artikelPrijs').val(result.artikelPrijs);
+        $("div#btnSave").val("Update"); //<--
     });
 }
 
-$(document).on("click", "div#btnSave button", function(event){
+$(document).on("click", "button#btnSave", function(event){
     event.preventDefault();
-    alert("Edit function not implemented");
-//	if ($('#artikelId').val() === '')
-//            alert("Edit function not implemented");
-//		create();
-//	else
-//            alert("Edit function not implemented");
+	if ($('#artikelId').val() === '')
+		create();
+	else
+            alert("Edit function not implemented");
 //		update();
-//	return false;
+	return false;
 });
 
+//$(document).on("click", "button#btnDelete", function(event){
+//    event.preventDefault();
+//	if ($('#artikelId').val() === '')
+//            alert("Selecteer eerst een artikel om te verwijderen");
+//	else
+//            alert("Delete function not implemented");
+////		delete();
+//	return false;
+//});
+
+
+    
 function create(){
 	$.ajax({
 		type: 'POST',
 		contentType: 'application/json',
 		url: rootURL,
-		dataType: "json",
+		dataType: "application/json",
 		data: formToJSON(),
 		success: function(data, textStatus, jqXHR){
 			alert('Artikel created successfully');
@@ -73,17 +102,34 @@ function create(){
 		}
 	});
         }
-function formToJSON() {
-	return JSON.stringify({
-		"id": $('#artikelId').val() === "" ? null : $('#artikelId').val(), //<--klopt dit?
-		"artikelNaam": $('#artikelNaam').val(), 
-		"artikelVoorraad": $('#artikelVoorraad').val(),
-		"artikelPrijs": $('#artikelPrijs').val()
+
+
+function deleteArtikel(Id){
+	$.ajax({
+		type: 'DELETE',
+		url: rootURL + '/' +Id,
+		success: function(data, textStatus, jqXHR){
+			alert('Artikel successfully deleted');
 		},
+		error: function(jqXHR, textStatus, errorThrown){
+			alert('deleteArtikel error: ' + textStatus);
+		}
+	});
+        $.getJSON(rootURL, function(result){
+            renderList(result);
+            });
+    }
+        
+function formToJSON() {
                 alert("yay: \n\
                         "+$('#artikelId').val() +" \n\
                         "+$('#artikelNaam').val()+ " \n\
                         "+$('#artikelVoorraad').val()+ " \n\
-                        "+$('#artikelPrijs').val())
-                        );
+                        "+$('#artikelPrijs').val()) ;
+ 
+	return JSON.stringify({
+		"artikelNaam": $('#artikelNaam').val(), 
+		"artikelVoorraad": $('#artikelVoorraad').val(),
+		"artikelPrijs": $('#artikelPrijs').val()
+		});
 }
